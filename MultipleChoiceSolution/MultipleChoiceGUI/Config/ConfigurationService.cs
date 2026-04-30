c0241c5d7c44ee575c0c927aad5d98b22fc0e0a4
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-30 11:57:31 +0200

    Set up Configuration Service class for easy config access

---
+++ MultipleChoiceSolution/MultipleChoiceGUI/Config/ConfigurationService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MultipleChoiceGUI.Config
+{
+    public static class ConfigurationService
+    {
+        private static IConfigurationRoot _config;
+        static ConfigurationService()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                           .AddJsonFile(@"./Config/appsettings.json", 
+                                           optional: false, reloadOnChange: true);
+            _config = builder.Build();
+        }
+
+        public static string GetConnectionString(string name)
+        {
+            return _config.GetConnectionString(name);
+        }
+
+        public static string GetSetting(string name)
+        {
+            return _config.GetSection("AppSettings")[name];
+        }
+    }
+}
