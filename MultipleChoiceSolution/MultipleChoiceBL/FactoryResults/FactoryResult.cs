caf64e82b6c3caa0ef23441374b4aaa1f7f05c09
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-30 12:33:31 +0200

    Add class for results of factories

---
+++ MultipleChoiceSolution/MultipleChoiceBL/FactoryResults/FactoryResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.FactoryResults
+{
+    public class FactoryResult<T>
+    {
+        public FactoryResult(T result)
+        {
+            Result = result;
+        }
+
+        public FactoryResult(List<string> errors)
+        {
+            Errors = errors;
+        }
+
+        public T? Result { get; private set; }
+        public List<String>? Errors { get; private set; }
+        public bool IsSucces { get => Errors == null; }
+
+
+    }
+}
