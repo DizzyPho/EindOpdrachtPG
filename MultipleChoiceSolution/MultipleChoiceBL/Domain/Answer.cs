76a7077abcce45c0df3d05f779b24685192e385b
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-30 12:44:11 +0200

    Add answer and question classes to domain

---
+++ MultipleChoiceSolution/MultipleChoiceBL/Domain/Answer.cs
@@ -0,0 +1,35 @@
+﻿using MultipleChoiceBL.FactoryResults;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.Domain
+{
+    public class Answer
+    {
+        private Answer(string answerText, bool isCorrect)
+        {
+            AnswerText = answerText;
+            IsCorrect = isCorrect;
+        }
+
+        public static bool TryCreate(string answerText, bool isCorrect, out FactoryResult<Answer> result)
+        {
+            List<String> errors = new List<string>();
+            if (string.IsNullOrWhiteSpace(answerText)) errors.Add("Answer cannot be empty.");
+
+            if (errors.Count > 0)
+            {
+                result = new FactoryResult<Answer>(errors);
+                return false;
+            }
+            else
+            {
+                result = new FactoryResult<Answer>(new Answer(answerText, isCorrect));
+                return false;
+            }
+        }
+        public string AnswerText { get; init; }
+        public bool IsCorrect { get; init; }
+    }
+}
---
+++ MultipleChoiceSolution/MultipleChoiceBL/Domain/Question.cs
@@ -0,0 +1,51 @@
+﻿using MultipleChoiceBL.FactoryResults;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.Domain
+{
+    public class Question
+    {
+        private Question(string questionText, List<Answer> answers)
+        {
+            QuestionText = questionText;
+            Answers = answers;
+        }
+        public static bool TryCreate(string questionText, List<Answer> answers, out FactoryResult<Question> result)
+        {
+            List<string> errors = new List<string>();
+
+            if (string.IsNullOrEmpty(questionText)) errors.Add("Question text cannot be empty.");
+            if (answers == null || answers.Count < 2)
+            {
+                errors.Add("Question must have at least 2 answers.");
+            }
+            else
+            {
+                if (!answers.Any(a => a.IsCorrect)) errors.Add("Please provide at least 1 correct answer.");
+            }
+
+            if (errors.Count > 0)
+            {
+                result = new FactoryResult<Question>(errors);
+                return false;
+            }
+            else
+            {
+                result = new FactoryResult<Question>(new Question(questionText, answers));
+                return true;
+            }
+        }
+
+        public string QuestionText { get; init; }
+
+        private List<Answer> Answers { get; init; }
+
+
+        public IReadOnlyList<Answer> GetAnswers()
+        {
+            return Answers;
+        }
+    }
+}
