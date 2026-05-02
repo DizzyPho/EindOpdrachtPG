0621a191cf73a0e573151d46d58a3da5b7c771d7
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-02 11:05:50 +0200

    Add quiz class

---
+++ MultipleChoiceSolution/MultipleChoiceBL/Domain/Quiz.cs
@@ -0,0 +1,46 @@
+﻿using MultipleChoiceBL.FactoryResults;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.Domain
+{
+    public class Quiz
+    {
+        private Quiz(string name, DateTime creationDate)
+        {
+            Name = name;
+            CreationDate = creationDate;
+            Questions = new List<Question>();
+        }
+
+        public static bool TryCreate(string name, DateTime creationDate, out FactoryResult<Quiz> result)
+        {
+            List<String> errors = new List<String>();
+
+            if (string.IsNullOrWhiteSpace(name)) errors.Add("Name cannot be empty.");
+            if (creationDate > DateTime.Now) errors.Add($"Invalid date '{creationDate}', cannot be in the future.");
+
+            if (errors.Count > 0)
+            {
+                result = new FactoryResult<Quiz>(errors);
+                return false;
+            }
+            else
+            {
+                result = new FactoryResult<Quiz>(new Quiz(name, creationDate));
+                return true;
+            }
+        }
+        public String Name { get; init; }
+
+        public DateTime CreationDate { get; init; }
+        public List<Question> Questions { get; init; }
+
+        public void AddQuestion(Question question)
+        {
+            Questions.Add(question);
+        }
+
+    }
+}
