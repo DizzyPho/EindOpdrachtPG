d59715c3f19b50a46b7a78a9d682240a179d2b1e
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-03 16:25:47 +0200

    Add new method tests

--- MultipleChoiceSolution/MultipleChoiceTest/TestAnswer.cs
+++ MultipleChoiceSolution/MultipleChoiceTest/TestAnswer.cs
@@ -1,5 +1,6 @@
 ﻿using MultipleChoiceBL.Domain;
 using MultipleChoiceBL.FactoryResults;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MultipleChoiceTest
 {
@@ -40,6 +41,38 @@ namespace MultipleChoiceTest
             Assert.NotNull(errors);
             Assert.Single(errors);
             Assert.Contains<String>("Answer cannot be empty.", errors);
+        }
+
+        [Fact]
+        public void Test_SetCorrect()
+        {
+            // check 1 : IsCorrect was false before
+            Answer.TryCreate("Test", false, out FactoryResult<Answer> factory);
+            Answer answer = factory.Result;
+
+            answer.SetCorrect();
+
+            Assert.True(answer.IsCorrect);
+
+            // check 2 : IsCorrect was already true
+            answer.SetCorrect();
+            Assert.True(answer.IsCorrect);
+        }
+
+        [Fact]
+        public void Test_SetIncorrect()
+        {
+            // check 1 : IsCorrect was true before
+            Answer.TryCreate("Test", true, out FactoryResult<Answer> factory);
+            Answer answer = factory.Result;
+
+            answer.SetIncorrect();
+
+            Assert.False(answer.IsCorrect);
+
+            // check 2 : IsCorrect was already true
+            answer.SetIncorrect();
+            Assert.False(answer.IsCorrect);
         }
     }
 
