580701d89e53173642d7b5fe46c3519c947b1e6e
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-03 15:10:19 +0200

    Add factory for filereaders

---
+++ MultipleChoiceSolution/MultipleChoiceUtil/Factories/FileReaderFactory.cs
@@ -0,0 +1,21 @@
+﻿using MultipleChoiceBL.Interfaces;
+using MultipleChoiceDL.FileReaders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceUtil.Factories
+{
+    public static class FileReaderFactory
+    {
+        public static IFileReader Create(string format)
+        {
+            IFileReader reader = format switch
+            {
+                "CorrectionAtEnd" => new FileReaderCorrectionAtEnd(),
+                _ => throw new ArgumentException("Invalid FileReader format")
+            };
+            return reader;
+        }
+    }
+}
