4aab7adc4b21e139ebfeb6304ed84d9f8523e48c
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-03 15:05:52 +0200

    Add interface for file readers

--- MultipleChoiceSolution/MultipleChoiceBL/Interfaces/IFileReader.cs
+++ MultipleChoiceSolution/MultipleChoiceBL/Interfaces/IFileReader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MultipleChoiceBL.Domain;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,6 @@ namespace MultipleChoiceBL.Interfaces
 {
     public interface IFileReader
     {
+        public List<Question> Read(string path);
     }
 }
