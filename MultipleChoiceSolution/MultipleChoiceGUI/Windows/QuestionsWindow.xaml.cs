d2bf00735d01764b56cfb843a535f85a441f748b
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-05 15:07:37 +0200

    Fill up topic combobox

--- MultipleChoiceSolution/MultipleChoiceGUI/Windows/QuestionsWindow.xaml.cs
+++ MultipleChoiceSolution/MultipleChoiceGUI/Windows/QuestionsWindow.xaml.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using MultipleChoiceBL.DTOs;
+using MultipleChoiceBL.Interfaces;
+using MultipleChoiceBL.Managers;
+using MultipleChoiceGUI.Config;
+using MultipleChoiceUtil.Factories;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,9 +24,15 @@ namespace MultipleChoiceGUI.Windows
     /// </summary>
     public partial class QuestionsWindow : Window
     {
+        Manager _manager;
+        ObservableCollection<TopicDTO> _topics;
         public QuestionsWindow()
         {
             InitializeComponent();
+            _manager = new Manager(RepoFactory.Create(ConfigurationService.GetConnectionString("SQLServerConnection"),
+                                                      ConfigurationService.GetSetting("databaseType")));
+            _topics = new ObservableCollection<TopicDTO>(_manager.GetTopics());
+            ComboBoxTopics.ItemsSource = _topics;
         }
 
         private void ButtonBack_Click(object sender, RoutedEventArgs e)
