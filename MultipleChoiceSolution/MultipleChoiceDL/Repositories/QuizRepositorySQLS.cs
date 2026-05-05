054496002866b939f8211e3db4f0ef52d8f5f408
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-05 09:01:17 +0200

    Add repository class

---
+++ MultipleChoiceSolution/MultipleChoiceDL/Repositories/QuizRepositorySQLS.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceDL.Repositories
+{
+    // MS SQL Server repository 
+    public class QuizRepositorySQLS
+    {
+    }
+}
