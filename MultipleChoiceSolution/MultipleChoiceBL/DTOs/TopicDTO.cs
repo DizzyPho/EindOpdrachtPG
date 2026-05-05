8d129949780ced2573348727bc4f3da1d445e1e2
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-05 14:51:23 +0200

    Add topicDTO class

---
+++ MultipleChoiceSolution/MultipleChoiceBL/DTOs/TopicDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.DTOs
+{
+    public class TopicDTO
+    {
+        public TopicDTO(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; init; }
+        public string Name { get; init; }
+
+    }
+}
