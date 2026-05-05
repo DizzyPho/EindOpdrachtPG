53847ce0cb4e9e5002b99d2c445e1c36907cac73
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-05 14:25:27 +0200

    Create window layout

--- MultipleChoiceSolution/MultipleChoiceGUI/MainWindow.xaml.cs
+++ MultipleChoiceSolution/MultipleChoiceGUI/MainWindow.xaml.cs
@@ -1,13 +1,10 @@
-﻿using System.Text;
+﻿using MultipleChoiceBL.Domain;
+using MultipleChoiceBL.Interfaces;
+using MultipleChoiceGUI.Config;
+using MultipleChoiceUtil.Factories;
+using System.Configuration;
+using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MultipleChoiceGUI
 {
@@ -19,6 +16,23 @@ namespace MultipleChoiceGUI
         public MainWindow()
         {
             InitializeComponent();
+            
+            IQuizRepository quizRepository = RepoFactory.Create(ConfigurationService.GetConnectionString("SQLServerConnection"),
+                                                                ConfigurationService.GetSetting("databaseType"));
+            IFileReader reader = FileReaderFactory.Create("CorrectionAtEnd");
+            //List<Question> q = reader.Read("./Data/Muziek80s.txt");
+            //quizRepository.ImportQuestions(q, [1,2,3]);
+
+        }
+
+        private void ButtonQuestions_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonQuiz_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
