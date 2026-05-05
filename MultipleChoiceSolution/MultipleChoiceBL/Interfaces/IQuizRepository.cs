06a0e6f53db2ec433d20a7bbbe51d3a35cd1673a
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-05 09:44:41 +0200

    Create interface for repositories

---
+++ MultipleChoiceSolution/MultipleChoiceBL/Interfaces/IQuizRepository.cs
@@ -0,0 +1,12 @@
+﻿using MultipleChoiceBL.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.Interfaces
+{
+    public interface IQuizRepository
+    {
+        public void ImportQuestions(List<Question> questions, int topicId);
+    }
+}
