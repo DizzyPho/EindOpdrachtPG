66f03b61fc805e4f0ac8b66a14a57362d72c067d
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-05 15:49:57 +0200

    Add QuestionDTO class

---
+++ MultipleChoiceSolution/MultipleChoiceBL/DTOs/QuestionDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.DTOs
+{
+    public record struct QuestionDTO
+    {
+        public QuestionDTO(int id, string question)
+        {
+            Id = id;
+            Question = question;
+        }
+
+        public int Id { get; set; }
+        public string Question { get; set; }
+
+    }
+}
