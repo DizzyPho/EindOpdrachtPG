7ab2d16b013bf60b945a930728fb46f99c3bb2d7
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-05 14:59:55 +0200

    Override tostring

--- MultipleChoiceSolution/MultipleChoiceBL/DTOs/TopicDTO.cs
+++ MultipleChoiceSolution/MultipleChoiceBL/DTOs/TopicDTO.cs
@@ -15,5 +15,9 @@ namespace MultipleChoiceBL.DTOs
         public int Id { get; init; }
         public string Name { get; init; }
 
+        public override string? ToString()
+        {
+            return Name;
+        }
     }
 }
