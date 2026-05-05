de9def8b04a08e5b9e74cf07a966ab28e219e0ca
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-05 11:43:23 +0200

    Add topic struct

---
+++ MultipleChoiceSolution/MultipleChoiceBL/Domain/Topic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.Domain
+{
+    public record struct Topic
+    {
+        public string Name { get; init; }
+        public List<String> Questions { get; init; }
+
+        public Topic(string name, List<String> questions)
+        {
+            Name = name;
+            Questions = questions;
+        }
+    }
+}
