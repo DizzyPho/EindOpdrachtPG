3dee0e7195b5559624d80ce113107b1c96a95237
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-05 09:46:49 +0200

    Create factory for repositories

---
+++ MultipleChoiceSolution/MultipleChoiceUtil/Factories/RepoFactory.cs
@@ -0,0 +1,21 @@
+﻿using MultipleChoiceBL.Interfaces;
+using MultipleChoiceDL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceUtil.Factories
+{
+    public static class RepoFactory
+    {
+        public static IQuizRepository Create(string connectionString, string type)
+        {
+            IQuizRepository repo = type switch
+            {
+                "SQLS" => new QuizRepositorySQLS(connectionString),
+                _ => throw new ArgumentException("Invalid DB type")
+            };
+            return repo;
+        }
+    }
+}
