fd9161f4afba327dffd7a719e74cb39e0c2b970c
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-05 14:15:31 +0200

    Add manager class

---
+++ MultipleChoiceSolution/MultipleChoiceBL/Managers/Manager.cs
@@ -0,0 +1,18 @@
+﻿using MultipleChoiceBL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.Managers
+{
+    public class Manager
+    {
+        private IQuizRepository _repository;
+        public Manager(IQuizRepository repository) 
+        {
+            _repository = repository;
+        }
+
+
+    }
+}
