ee4daa96cd24355f9530a49fb8483196fefc2f39
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-07 17:51:13 +0200

    Add back and confirm buttons

--- MultipleChoiceSolution/MultipleChoiceGUI/Windows/ImportQuestionsWindow.xaml.cs
+++ MultipleChoiceSolution/MultipleChoiceGUI/Windows/ImportQuestionsWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MultipleChoiceGUI.Windows
             InitializeComponent();
             DataContext = new ImportQuestionViewModel(manager);
         }
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
