34f498c9ab629840ca442791487b50684aec79bc
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-07 17:32:12 +0200

    Add viewmodel import window

---
+++ MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/ImportQuestion/ImportQuestionViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MultipleChoiceBL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceGUI.ViewModels.ImportQuestion
+{
+    public class ImportQuestionViewModel : BaseViewModel
+    {
+        public ImportQuestionViewModel(List<TopicDTO> topicList)
+        {
+            TopicList = topicList;
+        }
+        public List<TopicDTO> TopicList
+        {
+            get => Get<List<TopicDTO>>();
+            set => Set(value);
+        }
+
+        public String FilePath
+        {
+            get => Get<String>();
+            set => Set(value);
+        }
+    }
+}
