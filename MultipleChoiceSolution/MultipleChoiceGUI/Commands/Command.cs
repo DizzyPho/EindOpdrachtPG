f9c1f1eb55a20b2cf46419a03a95f2789a5b22d1
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-07 11:54:41 +0200

    Add command class of type ICommand

---
+++ MultipleChoiceSolution/MultipleChoiceGUI/Commands/Command.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace MultipleChoiceGUI.Commands
+{
+    internal class Command : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private readonly Action _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public Command(Action execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public Command(Action execute) : this(execute, (object obj) => true)
+        {
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+    }
+}
