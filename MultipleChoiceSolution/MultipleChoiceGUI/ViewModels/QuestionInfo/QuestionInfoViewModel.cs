1159055183070377011f77f14e22e3a065a8341d
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-07 11:54:29 +0200

    Remove command

---
+++ MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/QuestionInfo/QuestionInfoViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MultipleChoiceGUI.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MultipleChoiceGUI.ViewModels.QuestionInfo
+{
+    public class QuestionInfoViewModel : BaseViewModel
+    {
+        public String QuestionText
+        {
+            get => Get<String>();
+            set => Set(value);
+        }
+
+        public ICommand SelectedQuestionCommand { get; init; }
+
+        public QuestionInfoViewModel()
+        {
+            SelectedQuestionCommand = new Command(OnSelectedQuestionChange);
+        }
+
+        internal void OnSelectedQuestionChange()
+        {
+            MessageBox.Show("test");
+        }
+    }
+}
