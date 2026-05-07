405b27d661f66e089c100422a1e1ec357859a609
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-07 17:47:37 +0200

    Choose file

--- MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/ImportQuestion/ImportQuestionViewModel.cs
+++ MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/ImportQuestion/ImportQuestionViewModel.cs
@@ -1,8 +1,11 @@
-﻿using MultipleChoiceBL.DTOs;
+﻿using Microsoft.Win32;
+using MultipleChoiceBL.DTOs;
 using MultipleChoiceBL.Managers;
+using MultipleChoiceGUI.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace MultipleChoiceGUI.ViewModels.ImportQuestion
 {
@@ -13,6 +16,7 @@ namespace MultipleChoiceGUI.ViewModels.ImportQuestion
         {
             _manager = manager;
             TopicList = _manager.GetTopics();
+            SelectFileCommand = new Command(OnSelectFile);
         }
         public List<TopicDTO> TopicList
         {
@@ -24,6 +28,16 @@ namespace MultipleChoiceGUI.ViewModels.ImportQuestion
         {
             get => Get<String>();
             set => Set(value);
+        }
+
+        public ICommand SelectFileCommand { get; init; }
+
+        public void OnSelectFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            ofd.ShowDialog();
+            FilePath = ofd.FileName;
         }
     }
 }
