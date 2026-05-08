157b5954ed418b11772738495fed4aa1631467cc
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-08 10:01:43 +0200

    Add viewmodel for new question window

---
+++ MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/NewQuestion/NewQuestionViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MultipleChoiceBL.Managers;
+using MultipleChoiceGUI.Commands;
+using MultipleChoiceGUI.ViewModels.ImportQuestion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceGUI.ViewModels.NewQuestion
+{
+    public class NewQuestionViewModel : BaseViewModel
+    {
+        Manager _manager;
+        public NewQuestionViewModel(Manager manager)
+        {
+            _manager = manager;
+
+            TopicList = _manager.GetTopics()
+                                .Select(topic => new TopicViewModel(topic))
+                                .ToList();
+        }
+        public String QuestionText
+        {
+            get => Get<String>(); set => Set(value);
+        }
+        public List<TopicViewModel> TopicList
+        {
+            get => Get<List<TopicViewModel>>();
+            set => Set(value);
+        }
+    }
+}
