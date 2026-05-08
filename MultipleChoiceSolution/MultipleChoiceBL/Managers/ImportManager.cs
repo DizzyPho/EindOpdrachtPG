51527becc57a845402022ba98c523466b91e2770
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-08 08:56:33 +0200

    Add importmanager class

---
+++ MultipleChoiceSolution/MultipleChoiceBL/Managers/ImportManager.cs
@@ -0,0 +1,26 @@
+﻿using MultipleChoiceBL.Domain;
+using MultipleChoiceBL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.Managers
+{
+    public class ImportManager
+    {
+        private IQuizRepository _repository;
+        private IFileReader _reader;
+
+        public ImportManager(IQuizRepository repository, IFileReader reader)
+        {
+            _repository = repository;
+            _reader = reader;
+        }
+
+        public void ImportQuestions(string path, List<int> topicIds)
+        {
+            List<Question> questions = _reader.Read(path);
+            _repository.ImportQuestions(questions, topicIds);
+        }
+    }
+}
