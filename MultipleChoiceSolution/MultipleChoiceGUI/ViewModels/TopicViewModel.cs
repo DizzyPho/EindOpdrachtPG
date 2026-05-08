f3087cabf817c5c6e08dbe23e4a47a0bdb33e634
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-08 09:44:51 +0200

    Move class

--- MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/TopicViewModel.cs
+++ MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/TopicViewModel.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace MultipleChoiceGUI.ViewModels.ImportQuestion
+namespace MultipleChoiceGUI.ViewModels
 {
     public class TopicViewModel : BaseViewModel
     {
