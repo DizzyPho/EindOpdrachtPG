ff8e6878219dfad841d8f38b17ea2d8ef87445ea
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-09 12:30:16 +0200

    Remove creation date property

--- MultipleChoiceSolution/MultipleChoiceBL/Domain/Quiz.cs
+++ MultipleChoiceSolution/MultipleChoiceBL/Domain/Quiz.cs
@@ -7,20 +7,18 @@ namespace MultipleChoiceBL.Domain
 {
     public class Quiz
     {
-        private Quiz(string name, DateTime creationDate, int seed)
+        private Quiz(string name, int seed)
         {
             Name = name;
-            CreationDate = creationDate;
             Questions = new List<Question>();
             Seed = seed;
         }
 
-        public static bool TryCreate(string name, DateTime creationDate, int seed, List<Question> questions, out FactoryResult<Quiz> result)
+        public static bool TryCreate(string name, int seed, List<Question> questions, out FactoryResult<Quiz> result)
         {
             List<String> errors = new List<String>();
 
             if (string.IsNullOrWhiteSpace(name)) errors.Add("Name cannot be empty.");
-            if (creationDate > DateTime.Now) errors.Add($"Invalid date '{creationDate}', cannot be in the future.");
             if (questions.Count == 0 && questions == null) errors.Add("Add at least 1 question to the quiz.");
 
             if (errors.Count > 0)
@@ -30,13 +28,11 @@ namespace MultipleChoiceBL.Domain
             }
             else
             {
-                result = new FactoryResult<Quiz>(new Quiz(name, creationDate, seed));
+                result = new FactoryResult<Quiz>(new Quiz(name, seed));
                 return true;
             }
         }
         public String Name { get; init; }
-
-        public DateTime CreationDate { get; init; }
         public int Seed { get; init; }
         public List<Question> Questions { get; init; }
 
