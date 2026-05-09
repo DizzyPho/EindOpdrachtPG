faf1afa15a07a0b040f818d0bf27b58b17f1ec05
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-09 16:03:44 +0200

    Bind UI to viewmodels

---
+++ MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/NewQuiz/NewQuizViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MultipleChoiceBL.Managers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceGUI.ViewModels.NewQuiz
+{
+    public class NewQuizViewModel : BaseViewModel
+    {
+        Manager _manager;
+        public NewQuizViewModel(Manager manager)
+        {
+            _manager = manager;
+            TopicList = _manager.GetTopics()
+                                .Select(t =>  new TopicQuestionsAmountViewModel(t))
+                                .ToList();
+        }
+
+        public string Name 
+        {
+            get => Get<String>();
+            set => Set(value);
+        }
+        public List<TopicQuestionsAmountViewModel> TopicList { get; set; }
+    }
+}
---
+++ MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/NewQuiz/TopicQuestionsAmountViewModel.cs
@@ -0,0 +1,33 @@
+﻿using MultipleChoiceBL.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceGUI.ViewModels.NewQuiz
+{
+    public class TopicQuestionsAmountViewModel : BaseViewModel
+    {
+        public TopicQuestionsAmountViewModel(Topic topic)
+        {
+            Name = topic.Name;
+            IsChecked = false;
+        }
+
+        public bool IsChecked
+        {
+            get => Get<bool>();
+            set => Set(value);
+        }
+
+        public string Name { get; set; }
+        public int Count 
+        {
+            get => Get<int>();
+            set
+            {
+                Set(value);
+                IsChecked = value > 0;
+            }
+        }
+    }
+}
