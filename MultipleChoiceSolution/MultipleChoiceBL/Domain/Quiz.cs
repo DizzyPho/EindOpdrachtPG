00c7f4c73bf519a419a02712829bd6a558514173
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-09 12:22:09 +0200

    Take list of questions on creation

--- MultipleChoiceSolution/MultipleChoiceBL/Domain/Quiz.cs
+++ MultipleChoiceSolution/MultipleChoiceBL/Domain/Quiz.cs
@@ -11,16 +11,17 @@ namespace MultipleChoiceBL.Domain
         {
             Name = name;
             CreationDate = creationDate;
-            Questions = new HashSet<Question>();
+            Questions = new List<Question>();
             Seed = seed;
         }
 
-        public static bool TryCreate(string name, DateTime creationDate, int seed, out FactoryResult<Quiz> result)
+        public static bool TryCreate(string name, DateTime creationDate, int seed, List<Question> questions, out FactoryResult<Quiz> result)
         {
             List<String> errors = new List<String>();
 
             if (string.IsNullOrWhiteSpace(name)) errors.Add("Name cannot be empty.");
             if (creationDate > DateTime.Now) errors.Add($"Invalid date '{creationDate}', cannot be in the future.");
+            if (questions.Count == 0 && questions == null) errors.Add("Add at least 1 question to the quiz.");
 
             if (errors.Count > 0)
             {
@@ -37,13 +38,7 @@ namespace MultipleChoiceBL.Domain
 
         public DateTime CreationDate { get; init; }
         public int Seed { get; init; }
-        public HashSet<Question> Questions { get; init; }
-
-        public bool AddQuestion(Question question)
-        {
-            ArgumentNullException.ThrowIfNull(question);
-            return Questions.Add(question);
-        }
+        public List<Question> Questions { get; init; }
 
     }
 }
