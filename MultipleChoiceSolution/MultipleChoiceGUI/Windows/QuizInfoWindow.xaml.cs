19e5d90ed509a824ae6b13fe62f1734c46d21083
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-09 14:47:31 +0200

    Set datacontext

--- MultipleChoiceSolution/MultipleChoiceGUI/Windows/QuizInfoWindow.xaml.cs
+++ MultipleChoiceSolution/MultipleChoiceGUI/Windows/QuizInfoWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MultipleChoiceBL.Managers;
+using MultipleChoiceGUI.ViewModels.QuizInfo;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,9 +19,10 @@ namespace MultipleChoiceGUI.Windows
     /// </summary>
     public partial class QuizInfoWindow : Window
     {
-        public QuizInfoWindow()
+        public QuizInfoWindow(Manager manager)
         {
             InitializeComponent();
+            DataContext = new QuizInfoViewModel(manager);
         }
     }
 }
