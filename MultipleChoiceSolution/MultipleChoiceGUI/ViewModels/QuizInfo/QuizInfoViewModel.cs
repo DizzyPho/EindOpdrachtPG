5058861fc902d8876b93353aa7d8350c884193ec
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-09 14:32:50 +0200

    Add view model for quiz info window

---
+++ MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/QuizInfo/QuizInfoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using MultipleChoiceBL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MultipleChoiceGUI.ViewModels.QuizInfo
+{
+    public class QuizInfoViewModel : BaseViewModel
+    {
+        public ObservableCollection<QuizDTO> quizzes
+        {
+            get => Get<ObservableCollection<QuizDTO>>();
+            set => Set(value);
+        }
+
+        public QuizDTO SelectedQuiz
+        {
+            get => Get<QuizDTO>(); 
+            set => Set(value);
+        }
+    }
+}
