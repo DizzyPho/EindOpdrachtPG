54cc98cca80b560ca72a120efdab1b6163b95432
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-09 13:53:15 +0200

    Add quiz dto

---
+++ MultipleChoiceSolution/MultipleChoiceBL/DTOs/QuizDTO.cs
@@ -0,0 +1,23 @@
+﻿using MultipleChoiceBL.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.DTOs
+{
+    public class QuizDTO
+    {
+        public QuizDTO(int id, string name, int questionCount, List<Topic> topics)
+        {
+            Id = id;
+            Name = name;
+            QuestionCount = questionCount;
+            Topics = topics;
+        }
+
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public int QuestionCount { get; init; }
+        public List<Topic> Topics { get; init; }
+    }
+}
