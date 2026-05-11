35adf8a8f2aea981b0029213175daa8655342751
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-11 16:57:55 +0200

    Add viewmodel that acn implement closing functionality

---
+++ MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/WindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MultipleChoiceGUI.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace MultipleChoiceGUI.ViewModels
+{
+    public class WindowViewModel : BaseViewModel
+    {
+        protected Action CloseAction { get; init; }
+        public ICommand CloseCommand { get; init; }
+        public WindowViewModel(Action closeAction)
+        {
+            CloseAction = closeAction;
+            CloseCommand = new Command(CloseAction);
+        }
+    }
+}
