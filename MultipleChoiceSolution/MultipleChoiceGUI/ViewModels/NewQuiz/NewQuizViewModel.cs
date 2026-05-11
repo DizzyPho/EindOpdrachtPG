23162a69f729bda1b5ab1f948648ec46ffdbe7b0
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-11 11:30:09 +0200

    Generate test on button press

--- MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/NewQuiz/NewQuizViewModel.cs
+++ MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/NewQuiz/NewQuizViewModel.cs
@@ -1,16 +1,21 @@
-﻿using MultipleChoiceBL.Managers;
+﻿using MultipleChoiceBL.Domain;
+using MultipleChoiceBL.Managers;
+using MultipleChoiceGUI.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace MultipleChoiceGUI.ViewModels.NewQuiz
 {
     public class NewQuizViewModel : BaseViewModel
     {
         Manager _manager;
+        public ICommand GenerateQuizCommand { get; init; }
         public NewQuizViewModel(Manager manager)
         {
             _manager = manager;
+            GenerateQuizCommand = new Command(OnGenerateQuiz);
             TopicList = _manager.GetTopics()
                                 .Select(t =>  new TopicQuestionsAmountViewModel(t))
                                 .ToList();
@@ -22,5 +27,14 @@ namespace MultipleChoiceGUI.ViewModels.NewQuiz
             set => Set(value);
         }
         public List<TopicQuestionsAmountViewModel> TopicList { get; set; }
+
+        private void OnGenerateQuiz()
+        {
+            Dictionary<Topic, int> questionAmountByTopic = TopicList.Where(t => t.IsChecked)
+                                                                    .Select(t => new KeyValuePair<Topic, int>(t.Topic, t.Count))
+                                                                    .ToDictionary();
+          
+            _manager.GenerateQuiz(questionAmountByTopic, Name);
+        }
     }
 }
