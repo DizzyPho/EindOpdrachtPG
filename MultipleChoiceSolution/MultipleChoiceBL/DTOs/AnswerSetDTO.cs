0d92dc5730bc6c93bf1a060d5bf221c14c16808e
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-12 12:19:26 +0200

    Remove unused property

--- MultipleChoiceSolution/MultipleChoiceBL/DTOs/AnswerSetDTO.cs
+++ MultipleChoiceSolution/MultipleChoiceBL/DTOs/AnswerSetDTO.cs
@@ -8,12 +8,10 @@ namespace MultipleChoiceBL.DTOs
     {
         public AnswerSetDTO(int quizId, int userId, Dictionary<int, List<int>> questionAnswers)
         {
-            QuizId = quizId;
             UserId = userId;
             QuestionAnswers = questionAnswers;
         }
 
-        public int QuizId { get; set; }
         public int UserId { get; set; }
         public Dictionary<int, List<int>> QuestionAnswers { get; set; }
     }
