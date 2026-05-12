f19559d764f4c624d1a4bb1f56a5ca47880155f6
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-12 13:46:55 +0200

    Create interface

---
+++ MultipleChoiceSolution/MultipleChoiceGUI/Interfaces/IActionableWindow.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceGUI.Interfaces
+{
+    public interface IActionableWindow
+    {
+        public void CloseAction();
+    }
+}
