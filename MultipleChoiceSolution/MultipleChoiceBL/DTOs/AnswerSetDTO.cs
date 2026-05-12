1ef3660e9139880c71896f0578c295b4d8a46b72
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-12 11:59:32 +0200

    Add answerSetDTO class

---
+++ MultipleChoiceSolution/MultipleChoiceBL/DTOs/AnswerSetDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.DTOs
+{
+    public class AnswerSetDTO
+    {
+        public AnswerSetDTO(int quizId, int userId, Dictionary<int, List<int>> questionAnswers)
+        {
+            QuizId = quizId;
+            UserId = userId;
+            QuestionAnswers = questionAnswers;
+        }
+
+        public int QuizId { get; set; }
+        public int UserId { get; set; }
+        public Dictionary<int, List<int>> QuestionAnswers { get; set; }
+    }
+}
