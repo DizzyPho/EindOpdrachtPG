bc155619f701b7e48f69e6bb3dedc990923974bf
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-13 13:16:26 +0200

    Add submit command

--- MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/SolveQuiz/QuizViewModel.cs
+++ MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/SolveQuiz/QuizViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using MultipleChoiceBL.Domain;
+using MultipleChoiceBL.DTOs;
 using MultipleChoiceBL.Managers;
 using MultipleChoiceGUI.Commands;
 using MultipleChoiceGUI.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace MultipleChoiceGUI.ViewModels.SolveQuiz
 {
@@ -20,8 +22,27 @@ namespace MultipleChoiceGUI.ViewModels.SolveQuiz
 
             Questions = quiz.Questions.Select(q => new FullQuestionViewModel(q))
                                       .ToList();
+
+            SubmitCommand = new Command(OnSubmit);
         }
         public List<FullQuestionViewModel> Questions { get; init; }
         public String Title { get; init; }
+        public int UserId 
+        {
+            get => Get<int>();
+            set => Set(value);
+        }
+
+        public ICommand SubmitCommand { get; init; }
+
+        public void OnSubmit()
+        {
+            List<int> selectedAnswerIds = Questions.SelectMany(q => q.Answers)
+                                                   .Where(a => a.IsChecked)
+                                                   .Select(a => a.Id)
+                                                   .ToList();
+            AnswerSetDTO answerSet = new AnswerSetDTO(UserId, selectedAnswerIds);
+            _manager.SubmitAnswers(answerSet);
+        }
     }
 }
