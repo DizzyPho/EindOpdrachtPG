c11828c9147e8d3c9b4eafdad033bdb8c830b22a
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-14 13:40:33 +0200

    Add command for submitting answers in bulk

--- MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/BulkAnswer/BulkAnswerViewModel.cs
+++ MultipleChoiceSolution/MultipleChoiceGUI/ViewModels/BulkAnswer/BulkAnswerViewModel.cs
@@ -1,27 +1,38 @@
 ﻿using MultipleChoiceBL.Domain;
 using MultipleChoiceBL.DTOs;
 using MultipleChoiceBL.Managers;
+using MultipleChoiceGUI.Commands;
 using MultipleChoiceGUI.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace MultipleChoiceGUI.ViewModels.BulkAnswer
 {
     public class BulkAnswerViewModel : WindowViewModel
     {
         Manager _manager;
+        IActionableWindow _actionableWindow;
         Quiz _quiz;
         public BulkAnswerViewModel(int quizId, Manager manager, IActionableWindow actionableWindow) : base(actionableWindow)
         {
             _manager = manager;
             _quiz = _manager.GetQuiz(quizId);
+            _actionableWindow = actionableWindow;
+            SubmitCommand = new Command(OnSubmitAnswerSets);
         }
 
         public string BulkText
         {
             get => Get<String>();
             set => Set(value);
+        }
+        public ICommand SubmitCommand { get; init; }
+        public void OnSubmitAnswerSets()
+        {
+            _manager.SubmitAnswerSets(BulkTextToAnswerSets());
+            _actionableWindow.CloseAction();
         }
 
         public List<AnswerSetDTO> BulkTextToAnswerSets()
