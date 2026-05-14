4f42178b7893eb3634daa9574c5dcec11f9ff46e
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-14 12:25:30 +0200

    Add new window layout

---
+++ MultipleChoiceSolution/MultipleChoiceGUI/Windows/BulkAnswerWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MultipleChoiceGUI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MultipleChoiceGUI.Windows
+{
+    /// <summary>
+    /// Interaction logic for BulkAnswerWindow.xaml
+    /// </summary>
+    public partial class BulkAnswerWindow : Window, IActionableWindow
+    {
+        public BulkAnswerWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void CloseAction()
+        {
+            Close();
+        }
+    }
+}
