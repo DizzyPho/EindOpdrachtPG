d879910e4b0ceddc4e25ef7682acd3b2bf3ccb60
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-14 13:10:20 +0200

    Generate answerset from text

--- MultipleChoiceSolution/MultipleChoiceBL/DTOs/AnswerSetDTO.cs
+++ MultipleChoiceSolution/MultipleChoiceBL/DTOs/AnswerSetDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MultipleChoiceBL.Domain;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -14,5 +15,29 @@ namespace MultipleChoiceBL.DTOs
 
         public int UserId { get; set; }
         public List<int> AnswerIds { get; set; }
+
+        public static AnswerSetDTO StringToAnswerSet(Quiz quiz, string text)
+        {
+            string[] fields = text.Split(',');
+            int.TryParse(fields[0], out int userId);
+            string letters = fields[1];
+
+            List<Question> questions = quiz.Questions;
+            List<int> answerIds = new List<int>();
+
+            for (int i = 0; i < letters.Count(); i++)
+            {
+                int index = LetterToInt(letters[i]);
+                answerIds.Add((int)questions[i].GetAnswers()[index].Id);
+            }
+
+            return new AnswerSetDTO(userId, answerIds);
+        }
+
+        public static int LetterToInt(char letter)
+        {
+            const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return alphabet.IndexOf(letter);
+        }
     }
 }
