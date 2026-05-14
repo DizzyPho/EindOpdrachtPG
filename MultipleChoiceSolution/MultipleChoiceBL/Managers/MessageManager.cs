bf87c8c9b83e0f971fb7b0a53e9fcdd098f60276
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-14 14:11:15 +0200

    Add messagemaanger class

---
+++ MultipleChoiceSolution/MultipleChoiceBL/Managers/MessageManager.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.Managers
+{
+    public class MessageManager
+    {
+        public void Register<T>(object recipient, MessageHandler<object, T> handler)
+            where T : class
+        {
+            WeakReferenceMessenger.Default.Register<T>(recipient, handler);
+        }
+        public void Send<T>(T message) where T : class
+        {
+            WeakReferenceMessenger.Default.Send<T>(message);
+        }
+    }
+}
