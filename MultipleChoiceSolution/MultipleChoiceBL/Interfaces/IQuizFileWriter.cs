194e39f8702156bd3e6820cfc0d32c9438d8788c
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-15 10:55:31 +0200

    Add filewrtier

---
+++ MultipleChoiceSolution/MultipleChoiceBL/Interfaces/IQuizFileWriter.cs
@@ -0,0 +1,12 @@
+﻿using MultipleChoiceBL.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.Interfaces
+{
+    public interface IQuizFileWriter
+    {
+        public void SaveQuiz(Quiz quiz, string path);
+    }
+}
---
+++ MultipleChoiceSolution/MultipleChoiceDL/FileWriters/QuizFileWriterTXT.cs
@@ -0,0 +1,37 @@
+﻿using MultipleChoiceBL.Domain;
+using MultipleChoiceBL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceDL.FileWriters
+{
+    public class QuizFileWriterTXT : IQuizFileWriter
+    {
+        public void SaveQuiz(Quiz quiz, string path)
+        {
+
+            int questionNumber = 1;
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine(quiz.Name);
+                sw.WriteLine();
+
+                foreach (Question question in quiz.Questions)
+                {
+                    sw.WriteLine($"{questionNumber}. {question.QuestionText}");
+                    sw.WriteLine();
+
+                    char letter = 'A';
+                    foreach(Answer answer in question.GetAnswers())
+                    {
+                        sw.WriteLine($"{letter}. {answer.AnswerText}");
+                        letter++;
+                    }
+
+                    questionNumber++;
+                }
+            }
+        }
+    }
+}
