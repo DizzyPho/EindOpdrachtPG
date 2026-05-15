357ef38da25f59f4102f9258404ec43a51608039
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-15 11:33:19 +0200

    Create new message

---
+++ MultipleChoiceSolution/MultipleChoiceBL/Messages/QuestionsImportedMessage.cs
@@ -0,0 +1,19 @@
+﻿using MultipleChoiceBL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.Messages
+{
+    public class QuestionsImportedMessage
+    {
+        public QuestionsImportedMessage(List<int> topicIds, List<QuestionDTO> questions)
+        {
+            TopicIds = topicIds;
+            Questions = questions;
+        }
+
+        public List<int> TopicIds { get; init; }
+        public List<QuestionDTO> Questions { get; init; }
+    }
+}
