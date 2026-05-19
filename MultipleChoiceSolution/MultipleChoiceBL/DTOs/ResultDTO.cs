3e4469203c175b1be64b629612523dd43659d6b3
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-19 11:22:35 +0200

    Add result dto class

---
+++ MultipleChoiceSolution/MultipleChoiceBL/DTOs/ResultDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleChoiceBL.DTOs
+{
+    public class ResultDTO
+    {
+        public ResultDTO(int userId, int score)
+        {
+            UserId = userId;
+            Score = score;
+        }
+
+        public int UserId { get; init; }
+        public int Score { get; init; }
+    }
+}
