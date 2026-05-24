4fab7de827123cd165f86a9c1eaeea259bf0856a
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-24 10:59:08 +0200

    Update quiz tests

--- MultipleChoiceSolution/MultipleChoiceTest/TestQuiz.cs
+++ MultipleChoiceSolution/MultipleChoiceTest/TestQuiz.cs
@@ -11,15 +11,27 @@ namespace MultipleChoiceTest
     {
         Question testQuestion1;
         Question testQuestion2;
+        Question testQuestion3;
+
+        List<Question> NoQuestionsList;
+        List<Question> TwoQuestionsList;
+        List<Question> ThreeQuestionsList;
 
         public TestQuiz()
         {
             Answer.TryCreate("Antwoord1", true, out FactoryResult<Answer> answer1);
             Answer.TryCreate("Antwoord2", false, out FactoryResult<Answer> answer2);
+            Answer.TryCreate("Antwoord3", false, out FactoryResult<Answer> answer3);
             Question.TryCreate("Vraag1", [answer1.Result, answer2.Result], out var factoryResult);
             testQuestion1 = factoryResult.Result;
             Question.TryCreate("Vraag2", [answer1.Result, answer2.Result], out factoryResult);
             testQuestion2 = factoryResult.Result;
+            Question.TryCreate("Vraag3", [answer1.Result, answer3.Result], out factoryResult);
+            testQuestion3 = factoryResult.Result;
+
+            NoQuestionsList = new List<Question>();
+            TwoQuestionsList = [testQuestion1 , testQuestion2];
+            ThreeQuestionsList = [testQuestion1 , testQuestion2 , testQuestion3];
         }
 
         [Theory]
@@ -30,7 +42,7 @@ namespace MultipleChoiceTest
         [InlineData("2026-5-1", "Test 1 ")]
         public void Test_TryCreate_Name_Valid(DateTime creationDate, string name)
         {
-            bool isSucces = Quiz.TryCreate(name, creationDate, out  var factory);
+            bool isSucces = Quiz.TryCreate(name, 1, TwoQuestionsList, out  var factory);
 
             Quiz q = factory.Result;
             List<String> errors = factory.Errors;
@@ -50,7 +62,7 @@ namespace MultipleChoiceTest
         [InlineData("2026-5-1", "\n")]
         public void Test_TryCreate_Name_Invalid(DateTime creationDate, string name)
         {
-            bool isSucces = Quiz.TryCreate(name, creationDate, out var factory);
+            bool isSucces = Quiz.TryCreate(name, 1, TwoQuestionsList, out var factory);
 
             Quiz q = factory.Result;
             List<String> errors = factory.Errors;
@@ -61,86 +73,6 @@ namespace MultipleChoiceTest
             Assert.Single(errors);
             Assert.Contains<String>("Name cannot be empty.", errors);
         }
-        [Fact]
-        public void Test_TryCreate_Date_Valid()
-        {
-            // check 1 : today's date valid
-            DateTime creationDate = DateTime.Now;
-            bool isSucces = Quiz.TryCreate("Test 1", creationDate, out var factory);
-
-            Quiz q = factory.Result;
-            List<String> errors = factory.Errors;
-
-            Assert.True(isSucces);
-            Assert.NotNull(q);
-            Assert.Null(errors);
-            Assert.Equal(creationDate, q.CreationDate);
-            Assert.NotNull(q.Questions);
-
-            // check 2 : dates before today
-
-            creationDate = DateTime.Now.AddDays(-1);
-            isSucces = Quiz.TryCreate("Test 1", creationDate, out factory);
-
-            q = factory.Result;
-            errors = factory.Errors;
-
-            Assert.True(isSucces);
-            Assert.NotNull(q);
-            Assert.Null(errors);
-            Assert.Equal(creationDate, q.CreationDate);
-            Assert.NotNull(q.Questions);
-        }
-
-        [Fact]
-        public void Test_TryCreate_Date_Invalid()
-        {
-            DateTime creationDate = DateTime.Now.AddDays(1);
-            bool isSucces = Quiz.TryCreate("Test 1", creationDate, out var factory);
-
-            Quiz q = factory.Result;
-            List<String> errors = factory.Errors;
-
-            Assert.False(isSucces);
-            Assert.Null(q);
-            Assert.NotNull(errors);
-            Assert.Single(errors);
-            Assert.StartsWith("Invalid date", errors[0]);
-        }
-
-        [Fact]
-        public void Test_AddQuestion_Valid()
-        {
-            Quiz.TryCreate("Test 1", DateTime.Now, out var factory);
-            Quiz q = factory.Result;
-
-            // check 1 : add first question
-            Assert.True(q.AddQuestion(testQuestion1));
-            Assert.Single(q.Questions);
-            Assert.Contains(testQuestion1, q.Questions);
-
-            // check 2 : add a second question
-            Assert.True(q.AddQuestion(testQuestion2));
-            Assert.Equal(2, q.Questions.Count);
-            Assert.Contains(testQuestion2, q.Questions);
-        }
-
-        [Fact]
-        public void Test_AddQuestion_Invalid()
-        {
-            Quiz.TryCreate("Test 1", DateTime.Now, out var factory);
-            Quiz q = factory.Result;
-
-            // check 1 : add null question
-            Assert.Throws<ArgumentNullException>(() => q.AddQuestion(null));
-            Assert.Empty(q.Questions);
-
-            // check 2 : add duplicate question
-            q.AddQuestion(testQuestion1);
-
-            Assert.False(q.AddQuestion(testQuestion1));
-            Assert.Single(q.Questions);
-            Assert.Contains<Question> (testQuestion1, q.Questions);
-        }
+   
     }
 }
